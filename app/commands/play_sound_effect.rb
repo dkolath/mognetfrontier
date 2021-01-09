f243d9f5be300123288e1edc6500a91569473833
Daniel Kolath <danielkolath@yahoo.com>
2021-01-09 01:12:10 -0800

    typo

--- app/commands/play_sound_effect.rb
+++ app/commands/play_sound_effect.rb
@@ -3,8 +3,8 @@ class PlaySoundEffect
   def self.call(bot, event, _args)
     Rails.logger.info("#{Rails.root}/assets/media/distant_words_victory.mp3")
     Rails.logger.info(Dir.pwd)
-    
-    Dir.new('.').each {|file| ails.logger.info file }
+
+    Dir.new('.').each {|file| Rails.logger.info file }
     channel = event.user.voice_channel
     return "You're not in any voice channel!" unless channel
     bot.voice_connect(channel)
