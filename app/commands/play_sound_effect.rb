ab618519ef85aef04741b6ddf493a1306edbb628
Daniel Kolath <danielkolath@yahoo.com>
2021-01-09 00:13:25 -0800

    Fix bad asset path

--- app/commands/play_sound_effect.rb
+++ app/commands/play_sound_effect.rb
@@ -2,12 +2,10 @@ class PlaySoundEffect
 
   def self.call(bot, event, _args)
 
-    Rails.logger.info("#{Rails.root}/app/assets/media/distant_words_victory.mp3")
-    Rails.logger.info(Dir.pwd)
     channel = event.user.voice_channel
     return "You're not in any voice channel!" unless channel
     bot.voice_connect(channel)
     voice_bot = event.voice
-    voice_bot.play_file("#{Rails.root}/app/assets/media/distant_words_victory.mp3")
+    voice_bot.play_file("#{Rails.root}/assets/media/distant_words_victory.mp3")
   end
 end
