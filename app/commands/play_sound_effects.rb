4f3b1295caa3fd0b736bf92f1df47d07f1468ce7
Daniel Kolath <danielkolath@yahoo.com>
2021-01-08 23:11:52 -0800

    basic bot

---
+++ app/commands/play_sound_effects.rb
@@ -0,0 +1,11 @@
+class PlaySoundEffects
+
+  def self.call(bot, event, _args)
+
+      channel = event.user.voice_channel
+      return "You're not in any voice channel!" unless channel
+      bot.voice_connect(channel)
+      voice_bot = event.voice
+      voice_bot.play_file("#{Rails.root}/app/assets/media/distant_words_victory.mp3")
+  end
+end
