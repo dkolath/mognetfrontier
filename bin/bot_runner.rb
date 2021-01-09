63ac45e59275e6ead096fbfea615af7e313accbd
Daniel Kolath <danielkolath@yahoo.com>
2021-01-08 23:15:40 -0800

    Fix cut and paste issue

--- bin/bot_runner.rb
+++ bin/bot_runner.rb
@@ -1,6 +1,6 @@
 bot = Discordrb::Commands::CommandBot.new token: ENV['DISCORD_COMMAND_TOKEN'], prefix: '!'
 
 bot.command :mog_raffle  do |event, *args| MogRaffle.(event, args); end
-bot.command :testing_sound  do |event, *args| MogRaffle.(bot, event, args); end
+bot.command :testing_sound  do |event, *args| PlaySoundEffect.(bot, event, args); end
 
 bot.run
