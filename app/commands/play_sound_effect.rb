88cc27849f43b5e18e70984c9caf87db241acecd
Daniel Kolath <danielkolath@yahoo.com>
2021-01-15 21:44:36 -0800

    update to sound effects

--- app/commands/play_sound_effect.rb
+++ app/commands/play_sound_effect.rb
@@ -1,14 +1,12 @@
 class PlaySoundEffect
 
-  def self.call(bot, event, _args)
-    Rails.logger.info("#{Rails.root}/app/assets/media/distant_words_victory.mp3")
-    Rails.logger.info(Dir.pwd)
-
-    Dir.new('.').each {|file| Rails.logger.info file }
+  def self.call(bot, event, args)
+    sound = args.first
     channel = event.user.voice_channel
     return "You're not in any voice channel!" unless channel
+
     bot.voice_connect(channel)
     voice_bot = event.voice
-    voice_bot.play_file("#{Rails.root}/app/assets/media/distant_words_victory.mp3")
+    voice_bot.play_file("#{Rails.root}/app/assets/media/#{sound}.mp3")
   end
 end
